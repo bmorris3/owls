5a83f135b808d299a5aaf05dba40d3c5572409e1
Brett Morris <brettmorris21@gmail.com>
2022-08-12 16:09:21 +0200

    path error fixed

--- docs/download_figures.py
+++ docs/download_figures.py
@@ -3,7 +3,7 @@ from zipfile import ZipFile
 
 figshare_url = "https://figshare.com/ndownloader/articles/20480538/versions/3"
 figshare_path_tmp = download_file(figshare_url, cache=False)
-figshare_path = 'docs/owls/targets/figshare_pngs'
+figshare_path = 'owls/targets/figshare_pngs'
 
 with ZipFile(figshare_path_tmp, 'r') as zip_ref:
     zip_ref.extractall(figshare_path)
